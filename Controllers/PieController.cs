3752cbae414ab74cedd1a47ea69f44b342a2bb69
unknown <petrenko1982w@gmail.com>
2020-08-07 14:51:30 +0300

    First Views not working well

---
+++ Controllers/PieController.cs
@@ -0,0 +1,25 @@
+﻿using BethanysPieShop.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BethanysPieShop.Controllers
+{
+    public class PieController : Controller
+    {
+        private readonly IPieRepository _pieRepository;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public PieController(IPieRepository pieRepository, ICategoryRepository categoryRepository)
+        {
+            _pieRepository = pieRepository;
+            _categoryRepository = categoryRepository;
+        }
+
+
+        public ViewResult List()
+        {
+            return View(_pieRepository.AllPies);
+        }
+    }
+}
